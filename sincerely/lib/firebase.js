15be0bfe522264d1aa8536dc9801c1d24af957eb
chairfu <willabeany@gmail.com>
2025-04-28 21:01:03 -0700

    connected firestore and created a tester in Textbox.tsx which we can change/rearrange later

---
+++ sincerely/lib/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkaBDZm7W6FwC2koG2iMDIDViaU7BYrxY",
+  authDomain: "sincerely-eb8a6.firebaseapp.com",
+  projectId: "sincerely-eb8a6",
+  storageBucket: "sincerely-eb8a6.firebasestorage.app",
+  messagingSenderId: "353310573294",
+  appId: "1:353310573294:web:06475f5fcae7cf98c369ff",
+  measurementId: "G-F0T1EB016R"
+};
+
+// Initialize Firebase + firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {app, db};
--- sincerely/src/app/components/submit/Textbox.tsx
+++ sincerely/src/app/components/submit/Textbox.tsx
@@ -1,7 +1,31 @@
+'use client';
+
+import { useState } from "react";
+import { db } from "../../../../lib/firebase";
+import {collection, addDoc, DocumentReference} from 'firebase/firestore';
+
 const Textbox = () => {
-    return (
-        <div className=""> Textbox </div>
-    );
+    const [entry, setEntry] = useState('');
+    
+        const handleAddEntry = async () => {
+            try {
+                const docRef = await addDoc(collection(db, 'entries'), {
+                    entry,
+                    createdAt: new Date()
+                });
+                console.log('entry added with ID: ', docRef.id);
+            } catch (e) {
+                console.error('error adding entry: ', e);
+            }
+            
+        };
+    
+        return (
+            <div className=""> 
+                <textarea value={entry} onChange={(e) => setEntry(e.target.value)} />
+                <button onClick={handleAddEntry}>Submit</button>
+            </div>
+        );
 };
 
 export default Textbox;
