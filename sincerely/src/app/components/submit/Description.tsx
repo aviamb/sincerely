28c01910b9fe0af10a0b33305d3b2752da4b02a8
Alice8625 <ylai062@ucr.edu>
2025-05-02 14:17:36 -0700

    implemented description component

--- sincerely/src/app/components/submit/Description.tsx
+++ sincerely/src/app/components/submit/Description.tsx
@@ -1,6 +1,9 @@
 const Description = () => {
     return (
-        <div className=""> Description </div>
+        <div className = "border-l-4 border-l-[#96ca7a] pl-1 w-66"> 
+            <div className = "font-bold"> write about it </div>
+            <div> anything you want to share someday but without directly telling anyone? (fully anonymous!) </div>
+        </div>
     );
 };
 
