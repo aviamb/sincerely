58cfb3a6a46e98d993c85eb4315ca1d60b03d7f0
Akshay Jay <akshayjay1722@gmail.com>
2025-05-07 00:17:27 -0700

    Added send button

--- sincerely/src/app/components/submit/SendButton.tsx
+++ sincerely/src/app/components/submit/SendButton.tsx
@@ -1,6 +1,20 @@
+"use client";
+
+import { useState } from 'react';
+
 const SendButton = () => {
+    const [sent, setSent] = useState(false);
+
     return (
-        <div className=""> SendButton </div>
+        <div>
+            {!sent ? (
+                <button onClick={() => setSent(true)} className="scale-30">
+                    <img src="/send_button.png" alt="Send Button" className="w-full h-full" />
+                </button>
+            ) : (
+                <span>Message sent</span>
+            )}
+        </div>
     );
 };
 
--- sincerely/src/app/page.tsx
+++ sincerely/src/app/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+import SendButton  from "./components/submit/SendButton";
+import './globals.css';
+
 const Home = () => {
   return (
-    <div className="logo">
-      <img src="./sincerely_star.svg" alt="Sincerely Logo" />
-      <h1>Sincerely...</h1>
+    <div className="">
+      <div className="w-519 h-106 flex justify-center items-center">
+        <img src="/sincerely_star.svg" alt="Sincerely Logo" className="w-59 h-59 rotate-344.6" />
+        <h1 className="text-center">Sincerely...</h1>
+      </div>
+      <div className="rounded-lg p-4 max-w-md mx-auto">
+      </div>
     </div>
   );
 };
