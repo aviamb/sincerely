f41e845ed99a8bab84018aff1f3f445ebceb13ec
randall-nguyen <rnguy137@ucr.edu>
2025-05-09 01:00:22 -0700

    implemented new tag bar

--- sincerely/src/app/components/entries/TagBar.tsx
+++ sincerely/src/app/components/entries/TagBar.tsx
@@ -1,6 +1,19 @@
+import LoveTag from "./loveTag"
+import ClassTag from "./classTag"
+import UcrTag from "./ucrTag"
+import OtherTag from "./otherTag"
+
 const TagBar = () => {
     return (
-        <div className=""> TagBar </div>
+        <div className="border-2 w-120 rounded-4xl border-gray-300">
+            <div className="flex justify-center gap-4 flex-row p-2"> 
+                <LoveTag />
+                <ClassTag />
+                <UcrTag />
+                <OtherTag />
+            </div>
+        </div>
+        
     );
 };
 
