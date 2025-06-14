ca843ffcc1f772274360402acff16b3bcd3a80fe
randall-nguyen <rnguy137@ucr.edu>
2025-06-14 01:06:05 -0700

    fix responsiveness

--- sincerely/src/app/page.tsx
+++ sincerely/src/app/page.tsx
@@ -3,17 +3,16 @@ import Description from "./components/submit/Description";
 
 const Home = () => {
   return (
-    <main className="min-h-screen flex items-center justify-center px-4 py-10">
-      <div className="flex flex-col md:flex-row items-start md:items-center gap-10 max-w-6xl w-full">
-        <div className="flex-1">
+    <div>
+      <nav className="flex items-center p-4">
+      </nav>
+      <div className="flex justify-center h-screen">
+        <div className="flex justify-center mt-8 mx-12">
           <TextboxContainer />
         </div>
-
-        <div className="md:w-[300px] text-sm text-gray-700">
-          <Description />
-        </div>
       </div>
-    </main>
+      
+    </div>
   );
 };
 
